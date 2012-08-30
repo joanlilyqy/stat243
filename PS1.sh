ff52470af0181223c63e607180cd7d419dc37330
Ying Qiao <yingqiao@eecs.berkeley.edu>
2012-08-30 16:44:44 -0700

    with clean data

--- PS1.sh
+++ PS1.sh
@@ -1,3 +1,14 @@
 #### Stat 243 Fall 2012
 #### Problem Set 1
+
+# Download .csv from FEC
 curl -o "CandidateSummary.csv" "http://www.fec.gov/data/CandidateSummary.do?format=csv"
+
+# Extract Senate, and then split into 'REP' and 'DEM'
+grep "\"S\"" CandidateSummary.csv | grep "\"REP\"" > REP.csv
+grep "\"S\"" CandidateSummary.csv | grep "\"DEM\"" > DEM.csv
+
+# Preprocessing before spliting fields
+sed 's/\([^",]\),/\1/g' REP.csv | sed 's/[$"]//g' > REPclean.csv
+sed 's/\([^",]\),/\1/g' DEM.csv | sed 's/[$"]//g' > DEMclean.csv
+
