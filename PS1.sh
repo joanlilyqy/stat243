d28f71971ef24f044c3d2dd729f92d80f8347843
Ying Qiao <yingqiao@eecs.berkeley.edu>
2012-08-30 17:58:43 -0700

    1.(a) done

--- PS1.sh
+++ PS1.sh
@@ -1,6 +1,7 @@
 #### Stat 243 Fall 2012
 #### Problem Set 1
 
+#### 1.(a)
 # Download .csv from FEC
 curl -o "CandidateSummary.csv" "http://www.fec.gov/data/CandidateSummary.do?format=csv"
 
@@ -12,3 +13,13 @@ grep "\"S\"" CandidateSummary.csv | grep "\"DEM\"" > DEM.csv
 sed 's/\([^",]\),/\1/g' REP.csv | sed 's/[$"]//g' > REPclean.csv
 sed 's/\([^",]\),/\1/g' DEM.csv | sed 's/[$"]//g' > DEMclean.csv
 
+# Sort on total contribution and display the richest five
+cut -d ',' -f 2,3,4,5,7,8,20 REPclean.csv | sort -n -r -t ',' -k 7 | head -n 5
+cut -d ',' -f 2,3,4,5,7,8,20 DEMclean.csv | sort -n -r -t ',' -k 7 | head -n 5
+
+rm *.csv
+
+#### 1.(b)
+curl -o "cn12.zip" "ftp://ftp.fec.gov/FEC/2012/cn12.zip"
+curl -o "indiv12.zip" "ftp://ftp.fec.gov/FEC/2012/indiv12.zip"
+tar -xvf cn12.zip,indiv12.zip  # get "cn.txt"
