e4c16c32e86072447cf8c567e139a088c81825dc
Ying Qiao <s243ba@stat.Berkeley.EDU>
2012-09-06 20:19:06 -0700

    1.1 writeup done

--- knitpdf.sh
+++ knitpdf.sh
@@ -1,6 +1,18 @@
+alias "ema"="emacs -nw"
+
+
 function knitpdf () {
+    # knit file in R
     R -e "library(knitr);knit('${1}.tex')"
-    pdflatex ${1}-out.tex
+    # create shell output
+    echo "\begin{lstlisting}" > ${2}.tex
+    ./${2}.sh >> ${2}.tex
+    echo "\end{lstlisting}" >> ${2}.tex
+    echo "\begin{lstlisting}" > ${3}.tex
+    ./${3}.sh >> ${3}.tex
+    echo "\end{lstlisting}" >> ${3}.tex
+    # convert to pdf
+    pdflatex --enable-pipes --shell-escape ${1}-out.tex
     rm -rf cache/ figure/
     rm -f *~ *.log *.aux
 }
