2ec7dd300917c00d24264b26ec7e2ceb9b21cd25
Ying Qiao <yingqiao@eecs.berkeley.edu>
2012-09-12 11:13:04 -0700

    R setup for PS2

---
+++ PS2.1.r
@@ -0,0 +1,116 @@
+# R
+#### Stat243
+#### Problem Set 2
+
+rm(list=ls(all=TRUE)) #remove all objects
+
+#### Generate objects test cases
+ob0 <- rnorm(1)
+ob1 <- rnorm(10)
+ob2 <- rnorm(100)
+ob3 <- rnorm(1000)
+ob4 <- rnorm(10000)
+ob5 <- rnorm(100000)
+ob6 <- rnorm(1000000)
+ob7 <- rnorm(10000000)
+
+# Aux Printing Fun:
+printUnit <- function (obj) {
+            	units = c("", "k", "M", "G"); digit = 10
+            	i = 1; while (obj >= 1024) { obj = obj / 1024; i = i + 1;}
+            	if (i == 1) { cat(format(obj,width = digit,justify = 'right'),"\n",sep = "") }
+            	else { cat(format(as.integer(obj),width = digit - 1,justify = 'right'),units[i],"\n",sep = "") }
+            }
+           
+printObj <- function (objList) { # print out the object list nicely
+            	wid = 20; digit = 10
+            	cat(format("object",width = wid),format("bytes",width = digit,justify = 'right'),"\n",sep = "")
+         	    for (i in 1:length(objList)) {
+         	  		cat(format(names(objList)[i],width = wid),format(objList[i],width = digit,justify = 'right'),"\n",sep = "")
+         	    }
+            }
+
+printObjPretty <- function (objList) { # print out the object list NICELY&PRETTY
+                  	wid = 20; digit = 10
+                  	cat(format("object",width = wid),format("bytes",width = digit,justify = 'right'),"\n",sep = "")
+         	          for (i in 1:length(objList)) {
+         	          	cat(format(names(objList)[i], width = wid))
+#        	          	print(object.size(get(names(objList)[i])), units = "auto") #class built-in method, messy
+         	          	printUnit(objList[i]) #self-defined method, exact match for PS eg
+         	          }
+                  }
+
+
+# Main Func:
+lsObj <- function (numls = 100, sizeB = 0, pretty = FALSE) {	
+					listLs <- ls(envir = parent.frame())
+					
+					tmpEnv <- NULL # store object sizes within a certain user-defined ENV
+					nameEnv <- NULL # store the ENV name and object names within ENV
+					tmpFun <- NULL # store object sizes within a function closure
+					nameFun <- NULL # store the FUN name and object names within FUN
+					for (item in listLs){
+						objitem <- get(item,envir = parent.frame())
+						if (is.environment(objitem)) {
+							listEnv <- ls(envir = objitem)
+							tmpEnv <- sapply(listEnv, function(x){object.size(get(x,envir = objitem))})
+#							for (tmpItem in listEnv) { # objects in user-defined ENV
+#                 tmpEnv <- c(tmpEnv, object.size(get(tmpItem,envir = objitem)))
+#							}
+							nameEnv <- sapply(listEnv, function(x){names(x) <- paste(item,"$",x,sep = "")})
+						}
+						if (is.function(objitem)){
+							listFun <- ls(environment(objitem))
+							for (tmpItem in listFun) { # objects in FUN closure
+#								tmpFun <- c(tmpFun, object.size(get(tmpItem,envir = environment(objitem))))
+							}
+#							nameFun <- c(nameFun, paste(item,"$",listFun,sep = ""))
+						}
+					}
+					names(tmpEnv) <- nameEnv
+					names(tmpFun) <- nameFun
+					
+         	tmp <- sapply(listLs, function(x){object.size(get(x,envir = parent.frame(n = 4)))}) #get all object sizes from in the called frame
+                                                                                              # FUN -> sapply -> lsObj -> call(lsObj) | n=4
+         	if (length(tmpEnv) > 0) { tmp <- c(tmp,tmpEnv) } #if there is use-defined ENV
+         	if (length(tmpFun) > 0) { tmp <- c(tmp,tmpFun) } #if there is FUN closure
+         	tmp <- tmp[tmp[] > sizeB] # find objects larger than sizeB(bytes)
+         	if (numls > length(tmp)) { numls = length(tmp) }
+         	tmp <- sort(tmp, decreasing = TRUE)[1:numls] # find the numls largest objects
+         	
+         	
+         	if (pretty == TRUE) { printObjPretty(tmp) } # print PRETTY
+         	else { printObj(tmp) }
+         }
+
+
+
+#### Test case
+# 1.(a)
+#cat("**********Test Case 1*************\nlsObj(5,64)\n")
+#lsObj(5,64)
+#cat("**********Test Case 2*************\nlsObj(sizeB = 1000)\n")
+#lsObj(sizeB = 1000)
+#cat("**********Test Case 3*************\nlsObj(numls = 3)\n")
+#lsObj(numls = 3)
+cat("**********Test Case 4*************\nlsObj(pretty = TRUE)\n")
+lsObj(pretty = TRUE)
+cat("**********Test Case 5*************\nlsObj() in testFunc()\n")
+testFunc <- function () {
+            	data <- rnorm(100000)
+            	myFun <- function(theta) { dist <- rnorm(theta); return(exp(dist / theta)); }
+	lsObj()
+}
+testFunc()
+cat("**********Test Case 6*************\nlsObj() in test testEx()\n")
+testEx <- function () {
+          	x <- rnorm(100000)
+          	e <- new.env(); e$a <- x # an object hidden in an environment
+#          	e2 <- new.env(); e2$a2 <- x; e2$b2 <- 52
+          	myFun <- with(list( data = x ), # an object hidden in a closure
+                          function(theta) { dist <- rdist(data); return(exp(dist / theta)); } )
+                myFun2 <- with(list( data2 = x ),
+                          function(theta2) { dist <- rdist(data); return(exp(dist / theta2)); } )
+	lsObj()
+}
+testEx()
