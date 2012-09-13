e085535cb6a03a12127884200b0da7a3b0bf718f
Ying Qiao <yingqiao@eecs.berkeley.edu>
2012-09-12 23:01:49 -0700

    start PS2.3 on SCF

--- PS2.3.r
+++ PS2.3.r
@@ -4,3 +4,6 @@
 #### 3. President speech analysis
 
 rm(list=ls(all=TRUE)) #remove all objects
+
+system("wget -q -O 'index_pres.html' 'http://www.presidency.ucsb.edu/sou.php#axzz265cEKp1a'")
+system("fromdos index_pres.html")
