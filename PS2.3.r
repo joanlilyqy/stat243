a4a6e3733d045473f60a0507a4719bf378c2237c
Ying Qiao <yingqiao@eecs.berkeley.edu>
2012-09-17 16:06:34 -0700

    PS2.3.r fix greedy match problem

--- PS2.3.r
+++ PS2.3.r
@@ -10,87 +10,95 @@ setwd("./html")
 # Download all html files
 #system("wget -q -O 'index_pres.html' 'http://www.presidency.ucsb.edu/sou.php#axzz265cEKp1a'")
 #system("fromdos index_pres.html")
-index.pres <- readLines('index_pres.html',warn=FALSE)
+indexPres <- readLines('index_pres.html',warn=FALSE)
 # get speech text source
-patTxt <- '\\s{16}<td width=\\"\\d{2}\\" align=\\"center\\" class=\\"doclist\\"><a href=\\"'
-index.pres <- index.pres[grep(patTxt,index.pres,perl = TRUE)]
-index.pres <- sapply(index.pres, function(x){gsub(patTxt,"",x)},USE.NAMES = FALSE)
-patTxt2 <- '\\">\\d{4}<\\/a>(\\*|)<\\/td>'
-index.pres <- sapply(index.pres, function(x){gsub(patTxt2,"",x)},USE.NAMES = FALSE)
+patUrl1 <- '\\s{16}<td width=\\"\\d{2}\\" align=\\"center\\" class=\\"doclist\\"><a href=\\"'
+indexPres <- indexPres[grep(patUrl1,indexPres,perl = TRUE)]
+indexPres <- sapply(indexPres, function(x){gsub(patUrl1,"",x)},USE.NAMES=FALSE)
+patUrl2 <- '\\">\\d{4}<\\/a>(\\*|)<\\/td>'
+indexPres <- sapply(indexPres, function(x){gsub(patUrl2,"",x)},USE.NAMES=FALSE)
 # get file id from the source url
-patUrl <- 'http:\\/\\/www\\.presidency\\.ucsb\\.edu\\/ws\\/index\\.php\\?pid='
-fileid <- sapply(index.pres, function(x){gsub(patUrl,"",x)},USE.NAMES = FALSE)
-print(fileid)
-fileid <- sapply(fileid, function(x){paste(x,".html",sep="")})
+patUrl3 <- 'http:\\/\\/www\\.presidency\\.ucsb\\.edu\\/ws\\/index\\.php\\?pid='
+fileid <- sapply(indexPres, function(x){gsub(patUrl3,"",x)},USE.NAMES=FALSE)
 # download all files and convert to unix
 #sapply(1:length(fileid),
-#       function(i){system(paste("wget -q -O '",fileid[i],"' '",index.pres[i],"'",sep=""));
+#       function(i){system(paste("wget -q -O '",fileid[i],"' '",indexPres[i],"'",sep=""));
 #                   system(paste("fromdos ",fileid[i],sep=""))})
+
+
+
 
 # one case for test OR test all files
 #ff <- readLines('44541.html')
-ff <- sapply(fileid, function(x){readLines(x, warn=FALSE)})
+ff <- sapply(fileid, function(x){readLines(paste(x,".html",sep=""), warn=FALSE)})
 
 # get the president name
-patName <- "^<title>(.*)<\\/title>"
-name.pres <- ff[grep(patName,ff,perl = TRUE)]
-name.pres <- sapply(name.pres, function(x){gsub(patName,"\\1",x)},USE.NAMES = FALSE)
-name.pres <- sapply(name.pres, function(x){return(unlist(strsplit(x, ":"))[1])},USE.NAMES = FALSE)
+patName <- "^<title>(.*?)<\\/title>"
+namePres <- ff[grep(patName,ff,perl = TRUE)]
+namePres <- sapply(namePres, function(x){gsub(patName,"\\1",x)},USE.NAMES=FALSE)
+namePres <- sapply(namePres, function(x){return(unlist(strsplit(x, ":"))[1])},USE.NAMES=FALSE)
+names(namePres) <- fileid
 # get the talk date
-patDate <- '^.*<tr><td.*><span.*>.*<\\/span><br><span class=\\"docdate\\">(.*)<\\/span><\\/td>'
-date.talk <- ff[grep(patDate,ff,perl = TRUE)]
-date.talk <- sapply(date.talk, function(x){gsub(patDate,"\\1",x)},USE.NAMES = FALSE)
-date.talk <- sapply(date.talk, function(x){gsub("^.*\\s","",x)},USE.NAMES = FALSE)
-# get the speech content text
-patText <- '^\\s{3}<.*><span class=\\"displaytext\\">(.*)<\\/span>.*<span.*<\\/span><.*>$'
+patDate <- '^.*<span class=\\"docdate\\">(.*?)<\\/span>.*$'
+dateTalk <- ff[grep(patDate,ff,perl = TRUE)]
+dateTalk <- sapply(dateTalk, function(x){gsub(patDate,"\\1",x)},USE.NAMES=FALSE)
+dateTalk <- sapply(dateTalk, function(x){gsub("^.*\\s","",x)},USE.NAMES=FALSE)
+names(dateTalk) <- fileid
+# get the speech content text and prune for nice-format print
+patText <- '^.*<span class=\\"displaytext\\">(.*?)<\\/span>.*$'
 speechVec <- ff[grep(patText,ff,perl = TRUE)]
-speechVec <- sapply(speechVec, function(x){gsub(patText,"\\1",x)},USE.NAMES = FALSE)
-patText2 <- '(.*)<div align.*<\\/div>'
-speechVec <- sapply(speechVec, function(x){gsub(patText2,"\\1<p>",x)},USE.NAMES=FALSE)
-# problem with ending <br>
-talk.len <- sapply(speechVec, function(x){nchar(as.character(x))},USE.NAMES=FALSE)
+speechVec <- sapply(speechVec, function(x){gsub(patText,"\\1",x)},USE.NAMES=FALSE) # grab speech text
+speechVec <- sapply(speechVec, function(x){gsub("(<p.*?>|<\\/p>|<br>)","\n",x)},USE.NAMES=FALSE) # for line ending
+speechVec <- sapply(speechVec, function(x){gsub("<.*?>","",x)},USE.NAMES=FALSE) # remove all html format
+speechVec <- sapply(speechVec, function(x){x <- gsub("&mdash;"," -- ",x);
+                                           x <- gsub("&nbsp;","  ",x);
+                                           x <- gsub("&lsquo;"," ' ",x);
+                                           x <- gsub("&#8226;"," \\. ",x);
+                                           x <- gsub("&lt;"," < ",x);
+                                           x <- gsub("&deg;"," degree ",x);
+                                           x <- gsub("&pound;"," pound ",x);                                           
+                                           x <- gsub("&fra.*?;"," 1/2 ",x);                                           
+                                           x <- gsub("&O.*?;","O",x);
+                                           x <- gsub("&e.*?;","e",x);                                     
+                                          },USE.NAMES=FALSE) # html special char
+names(speechVec) <- fileid
+# remove audience response tags (laughter & applause)
+patLau <- '\\[.*?(Laughter|laughter).*?\\]'
+patApp <- '\\[.*?(Applause|applause).*?\\]'
+getlauNum <- function(x) {
+          if (length(gregexpr(patLau,x,perl=TRUE)[[1]]) == 1 &&
+              gregexpr(patLau,x,perl=TRUE)[[1]] == -1)         { return(0) }
+          else { return(length(gregexpr(patLau,x,perl=TRUE)[[1]]))}
+          }
+getappNum <- function(x) {
+          if (length(gregexpr(patApp,x,perl=TRUE)[[1]]) == 1 &&
+              gregexpr(patApp,x,perl=TRUE)[[1]] == -1)         { return(0) }
+          else { return(length(gregexpr(patApp,x,perl=TRUE)[[1]]))}
+          }
+#getStr <- function(i){
+#          return(substring(speechVec[i],attr(tagIdx[[i]],"capture.start"),
+#                    attr(tagIdx[[i]],"capture.start")+attr(tagIdx[[i]],"capture.length")-1))
+#          }
+lauNum <- sapply(speechVec, getlauNum)
+appNum <- sapply(speechVec, getappNum)
+#tagIdx <- lapply(speechVec, function(x){return(gregexpr(patTag,x,perl=TRUE)[[1]])})
+#tagStr <- sapply(1:length(speechVec),getStr)
 
+speechVec <- sapply(speechVec, function(x){x <- gsub(patLau,"",x,perl=TRUE);
+                                           x <- gsub(patApp,"",x,perl=TRUE)})
 
 #debug
-#print(name.pres)
-#print(date.talk)
-#cat(length(name.pres)," names\n",head(name.pres),"\n")
-#cat(length(date.talk)," dates\n",head(date.talk),"\n")
-#cat(length(speechVec)," speeches\n")
-#print(speechVec[1]
-#for (i in 1:length(fileid)){
-#  ff <- readLines(fileid[i])
-#  print(i)
-#  print(grep(patText,ff,perl = TRUE))
-#}
-print(talk.len)
+#print(namePres)
+#print(dateTalk)
+#talk.len <- sapply(speechVec, function(x){nchar(as.character(x))})
+#print(talk.len)
+print(lauNum)
+print(appNum)
+#print(tagStr[1:25])
 
 
-patTag <- '\\[<i>(\\w*)<\\/i>\\]'
-getNum <- function(x) {
-          if (length(gregexpr(patTag,x,perl=TRUE)[[1]]) == 1 &&
-              gregexpr(patTag,x,perl=TRUE)[[1]] == -1)         { return(0) }
-          else { return(length(gregexpr(patTag,x,perl=TRUE)[[1]]))}
-          }
-getStr <- function(i){
-          return(substring(speechVec[i],attr(tagIdx[[i]],"capture.start"),
-                    attr(tagIdx[[i]],"capture.start")+attr(tagIdx[[i]],"capture.length")-1))
-          }
-tagNum <- sapply(speechVec, getNum,USE.NAMES=FALSE)
-tagIdx <- lapply(speechVec, function(x){return(gregexpr(patTag,x,perl=TRUE)[[1]])})
-tagStr <- sapply(1:length(speechVec),getStr)
-
-print(tagNum)
-# problem with no <i> for the tags
-print(tagStr[1:25])
-
-speechVec <- sapply(speechVec, function(x){iconv(x,from="WINDOWS-1251",to="UTF-8")})
-speechVec <- sapply(speechVec, function(x){gsub(patTag,"",x,perl=TRUE)},USE.NAMES=FALSE)
-speechVec <- sapply(speechVec, function(x){gsub("<p>","\n",x)},USE.NAMES=FALSE)
-# still have html special char
-
-#sapply(speechVec, function (x){cat("START\n\n",x,"\n\n\n\n\nEND\n\n")}, USE.NAMES=FALSE)
-
+speechVec <- sapply(speechVec, function(x){iconv(x,from="WINDOWS-1251",to="UTF-8",sub = " ")})
+sapply(1:length(speechVec), function (i){cat("START:",fileid[i],"\n",speechVec[i],"\n\nEND\n\n",file="all.html",append=TRUE)}, USE.NAMES=FALSE)
 
 
 
