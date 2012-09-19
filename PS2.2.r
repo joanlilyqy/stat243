1272f999abad0cd3561764183f4d0750d077ff67
Ying Qiao <yingqiao@eecs.berkeley.edu>
2012-09-18 21:22:19 -0700

    PS2.2 writeup done

--- PS2.2.r
+++ PS2.2.r
@@ -9,14 +9,20 @@ load('IPs.RData') # import text data
 #patIP <- "([[:digit:]]{1,3}\\.){3}[[:digit:]]{1,3}" #POXIS sytle pattern for IP
 patIP <- "((\\d{1,3}\\.){3}\\d{1,3})" #Perl style pattern for IP
 
-getIPnum <- function (t) { if (gregexpr(patIP,t,perl=TRUE)[[1]] == -1) {return (0)}
-                           else {return (length(gregexpr(patIP,t,perl=TRUE)[[1]]))}
+getIPnum <- function (t) { if (length(grep(patIP,t,perl=TRUE)) == 0) {return (0)}
+                           else {
+                           return (length(gregexpr(patIP,t,perl=TRUE)[[1]]))
+                           }
                          } # get # of IPs per text element
 getIPidx <- function (t) { return (gregexpr(patIP,t,perl=TRUE)[[1]]) } # get index of IPs in text element
 getIPsub <- function (i) { return (substring(text[i],ipIdx[[i]],ipIdx[[i]]+attr(ipIdx[[i]],"match.length")-1))} # use idx to get IP strings
 
-ipNum <- sapply(text, getIPnum, USE.NAMES = FALSE) # 2.(b)
-ipIdx <- lapply(text, getIPidx) # maintain list structure of index
-ipStr <- sapply(1:length(text), getIPsub) # obtain IPs in list, multiple IPs per element stored in list items
+ipNum <- sapply(text, getIPnum, USE.NAMES = FALSE) # apply to the character vector
+ipIdx <- lapply(text, getIPidx) # maintain list structure of index for each IP log
+ipStr <- sapply(1:length(text), getIPsub)  # obtain IPs in list, multiple IPs per element stored in list items
 
-lapply(ipStr, write, "IPs.txt", append = TRUE)
+names(ipStr) <- text
+cat("The results of # of IPs in each text element (1:100):\n")
+print(ipNum[1:100])
+cat("The first 10 lines of results:\n")
+print(head(ipStr, n= 10))
