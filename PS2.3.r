1e01f86127f1064c427da98f0e443b12ec2662a4
Ying Qiao <yingqiao@eecs.berkeley.edu>
2012-09-18 17:01:21 -0700

    PS2.3 updated with plots

--- PS2.3.r
+++ PS2.3.r
@@ -4,43 +4,40 @@
 #### 3. President speech analysis
 
 rm(list=ls(all=TRUE)) #remove all objects
-#system("mkdir html")
+system("mkdir html")
 setwd("./html")
 
 # Download all html files
-#system("wget -q -O 'index_pres.html' 'http://www.presidency.ucsb.edu/sou.php#axzz265cEKp1a'")
-#system("fromdos index_pres.html")
+system("wget -q -O 'index_pres.html' 'http://www.presidency.ucsb.edu/sou.php#axzz265cEKp1a'")
+system("fromdos index_pres.html")
 indexPres <- readLines('index_pres.html',warn= FALSE)
-# get speech text source
+# Get speech text source
 patUrl1 <- '\\s{16}<td width=\\"\\d{2}\\" align=\\"center\\" class=\\"doclist\\"><a href=\\"'
 indexPres <- indexPres[grep(patUrl1,indexPres,perl= TRUE)]
 indexPres <- sapply(indexPres, function(x){gsub(patUrl1,"",x)},USE.NAMES= FALSE)
 patUrl2 <- '\\">\\d{4}<\\/a>(\\*|)<\\/td>'
 indexPres <- sapply(indexPres, function(x){gsub(patUrl2,"",x)},USE.NAMES= FALSE)
-# get file id from the source url
+# Get file id from the source url
 patUrl3 <- 'http:\\/\\/www\\.presidency\\.ucsb\\.edu\\/ws\\/index\\.php\\?pid='
 fileid <- sapply(indexPres, function(x){gsub(patUrl3,"",x)},USE.NAMES= FALSE)
-# download all files and convert to unix
-#sapply(1:length(fileid),
-#       function(i){system(paste("wget -q -O '",fileid[i],"' '",indexPres[i],"'",sep=""));
-#                   system(paste("fromdos ",fileid[i],sep=""))})
+# Download all files and convert to unix
+sapply(1:length(fileid),
+       function(i){system(paste("wget -q -O '",fileid[i],"' '",indexPres[i],"'",sep=""));
+                   system(paste("fromdos ",fileid[i],sep=""))})
 
-
-# one case for test OR test all files
-#ff <- readLines('44541.html')
 ff <- sapply(fileid, function(x){readLines(paste(x,".html",sep=""), warn= FALSE)})
 
-# get the president name
+# Get the president name
 patName <- "^<title>(.*?)<\\/title>"
 namePres <- ff[grep(patName,ff,perl= TRUE)]
 namePres <- sapply(namePres, function(x){gsub(patName,"\\1",x)},USE.NAMES= FALSE)
 namePres <- sapply(namePres, function(x){return(unlist(strsplit(x, ":"))[1])},USE.NAMES= FALSE)
-# get the talk date
+# Get the talk date
 patDate <- '^.*<span class=\\"docdate\\">(.*?)<\\/span>.*$'
 dateTalk <- ff[grep(patDate,ff,perl= TRUE)]
 dateTalk <- sapply(dateTalk, function(x){gsub(patDate,"\\1",x)},USE.NAMES= FALSE)
 dateTalk <- sapply(dateTalk, function(x){gsub("^.*\\s","",x)},USE.NAMES= FALSE)
-# get the speech content text and prune for nice-format print
+# Get the speech content text and prune for nice-format print
 patText <- '^.*<span class=\\"displaytext\\">(.*?)<\\/span>.*$'
 speechVec <- ff[grep(patText,ff,perl= TRUE)]
 speechVec <- sapply(speechVec, function(x){gsub(patText,"\\1<p>",x)},USE.NAMES= FALSE) # grab speech text
@@ -57,7 +54,7 @@ speechVec <- sapply(speechVec, function(x){x <- gsub("&mdash;"," -- ",x);
                                            x <- gsub("&O.*?;","O",x);
                                            x <- gsub("&e.*?;","e",x);                                     
                                           },USE.NAMES= FALSE) # html special char
-# remove audience response tags (laughter & applause)
+# Remove audience response tags (laughter & applause)
 patLau <- '\\[.*?(Laughter|laughter).*?\\]'
 patApp <- '\\[.*?(Applause|applause).*?\\]'
 getlauNum <- function(x) {
@@ -76,11 +73,6 @@ appNum <- sapply(speechVec, getappNum, USE.NAMES= FALSE)
 speechVec <- sapply(speechVec, function(x){iconv(x,from="WINDOWS-1251",to="UTF-8",sub = " ")})
 speechVec <- sapply(speechVec, function(x){x <- gsub(patLau,"",x,perl= TRUE);
                                            x <- gsub(patApp,"",x,perl= TRUE)})
-#debug
-#talk.len <- sapply(speechVec, function(x){nchar(as.character(x))})
-#print(talk.len)
-#system("rm -f all.html")
-#sapply(1:length(speechVec), function (i){cat("START:",fileid[i],"\n",speechVec[i],"\n\nEND\n\n",file="all.html",append=TRUE)}, USE.NAMES=FALSE)
 names(speechVec) <- NULL
 listSpeech <- list()
 listSpeech$id <- fileid
@@ -109,7 +101,7 @@ tmpList <- matrix(rep(0, 226*15),nrow=226,ncol=15)
 
 # Speech list with element-wise analysis
 speechList <- list()
-#system("wget -q -O 'common_words.txt' 'http://www.textfixer.com/resources/common-english-words.txt'")
+system("wget -q -O 'common_words.txt' 'http://www.textfixer.com/resources/common-english-words.txt'")
 commonWords <- readLines('common_words.txt', warn= FALSE)
 commonWords <- unlist(strsplit(commonWords, ",", perl= TRUE))
 for (i in 1:length(fileid)) {
@@ -282,17 +274,8 @@ getDigts <- function(x){x <- gsub("(\\d)[,\\.](\\d)","\\1\\2",x, perl= TRUE);
                         return(xs[xs != ""])}
 listSpeech$dc <- sapply(speechVec, function(x){return(length(getDigts(x)))}, USE.NAMES= FALSE)
 typ="h"; col="blue"; lwd=6; 
-plot(date,dc, type=typ,col=col,lwd=lwd, main=plotTitle1,xlab=xlab,ylab="Digits Count")
-
+plot(date,listSpeech$dc, type=typ,col=col,lwd=lwd, main=plotTitle1,xlab=xlab,ylab="Digits Count")
 
 
 setwd("../")
-#system("rm -rf html")
-
-#(i) and (for presidents since Franklin Roosevelt in 1932) whether they seem to differ between Republican and
-#Democratic presidents 
-# Your response here does not have to be extensive.
-
-#(j) Extra credit: Do some additional research and/or additional thinking to come up with additional
-#variables that quantify speech in interesting ways. Do some plotting that illustrates how the
-#speeches have changed over time.
+system("rm -rf html")
