e623d1268a4d90a584dd7f9e7be8a6dc8e584048
Ying Qiao <yingqiao@eecs.berkeley.edu>
2012-12-09 01:26:28 -0800

    PS5.4 start

--- PS5/PS5.4.r
+++ PS5/PS5.4.r
@@ -5,3 +5,18 @@
 
 rm(list = ls(all = TRUE)) # remove all objects
 
+theta <- rep(NA, 3)
+names(theta) <- c("beta0", "beta1", "sigma2")
+
+theta["beta0"]  <- 0.2
+theta["beta1"]  <- 2
+theta["sigma2"] <- 0.1
+
+n <- 100
+c <- n * 0.2 # test (a) exceedance=20%
+
+
+x <- runif(n)
+y <- rnorm(n, mean = theta["beta0"]+theta["beta1"]*x, sd = sqrt(theta["sigma2"]))
+
+## (c) EM implementation
